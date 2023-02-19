fc29e66e0e675eb9899020084f4f9016bfad9f50
Vivek Sharma <viveksharma1282003@gmail.com>
2023-02-19 16:18:19 +0530

    'update'

--- client/script.js
+++ client/script.js
@@ -95,7 +95,7 @@ const handleSubmit = async (e) =>{
 
   // fetch data from server 
 
-  const response = await fetch('https://jack-bot-sexr.onrender.com', {
+  const response = await fetch('http://localhost:5000/', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
