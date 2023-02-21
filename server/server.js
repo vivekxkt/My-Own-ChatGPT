10820dea38988c1b972268160073c39eb666f7d3
Vivek <82776809+vivekxkt@users.noreply.github.com>
2023-02-21 20:38:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.post('/',async (req,res)=>{
             model : "text-davinci-003",
             prompt : `${prompt}`,
             temperature : 1,
-            max_tokens : 10000,
+            max_tokens : 8000,
             top_p : 0.9,
             frequency_penalty : 0.1,
             presence_penalty : 0.1 ,
