88ed48866bf1c1ff2368d86b5e437360aa6cd3b7
Vivek <82776809+vivekxkt@users.noreply.github.com>
2023-02-23 17:42:38 +0530

    Delete design.js

--- client/design.js
+++
@@ -1,8 +0,0 @@
-
-
-const themeBtn = document.querySelector('.theme-btn');
-
-themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('theme-dark');
-    document.body.classList.toggle('theme-light');
-});
